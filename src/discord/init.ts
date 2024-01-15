b26b573a664dfaa49544b3bc52fc0e75e26cf018
Adam <adam@adamlearns.live>
2024-01-15 14:01:28 -0800

    Refactor Discord init function

--- src/discord/init.ts
+++ src/discord/init.ts
@@ -1,4 +1,10 @@
-import { Client, Events, GatewayIntentBits, roleMention } from "discord.js"
+import {
+  Client,
+  Events,
+  GatewayIntentBits,
+  roleMention,
+  type Interaction,
+} from "discord.js"
 
 import { emitter } from "../events/emitter.js"
 
@@ -6,7 +12,7 @@ import { readAllCommands, type Command } from "./commands.js"
 
 type ClientWithCommands = Client & { Commands: Map<string, Command> }
 
-export async function init() {
+function ensureEnvVars() {
   if (
     process.env.DISCORD_TOKEN === undefined ||
     process.env.DISCORD_STREAM_ANNOUNCEMENTS_CHANNEL_ID === undefined ||
@@ -14,51 +20,35 @@ export async function init() {
   ) {
     throw new Error("Missing environment variables for Discord.")
   }
+}
 
-  const announcementsChannelId =
-    process.env.DISCORD_STREAM_ANNOUNCEMENTS_CHANNEL_ID
-
-  const streamAnnouncementsRoleId =
-    process.env.DISCORD_STREAM_ANNOUNCEMENTS_ROLE_ID
+export async function init() {
+  ensureEnvVars()
 
   const client = new Client({ intents: [GatewayIntentBits.Guilds] })
   const clientWithCommands = client as ClientWithCommands
 
   clientWithCommands.Commands = await readAllCommands()
+  clientWithCommands.once(Events.ClientReady, onClientReady(clientWithCommands))
 
-  clientWithCommands.once(Events.GuildCreate, (guild) => {
-    console.log(`Joined the ${guild.name} server.`)
-  })
+  const announcementsChannelId = process.env
+    .DISCORD_STREAM_ANNOUNCEMENTS_CHANNEL_ID as string
+  const streamAnnouncementsRoleId = process.env
+    .DISCORD_STREAM_ANNOUNCEMENTS_ROLE_ID as string
+  emitter.onStreamOnline(
+    onStreamOnline(announcementsChannelId, client, streamAnnouncementsRoleId),
+  )
 
-  clientWithCommands.once(Events.ClientReady, (readyClient) => {
-    const allGuildNames = readyClient.guilds.cache.map((guild) => guild.name)
-    const allGuildNamesString = JSON.stringify(allGuildNames)
-    const botName = readyClient.user.tag
-    const numCommands = clientWithCommands.Commands.size
-    console.log(
-      `Logged in on Discord as ${botName} to servers: ${allGuildNamesString}. Read in ${numCommands} command(s).`,
-    )
-  })
+  clientWithCommands.on(
+    Events.InteractionCreate,
+    onInteraction(clientWithCommands),
+  )
 
-  emitter.onStreamOnline(async (title) => {
-    console.log("Stream went online:", title)
-    const channelId = announcementsChannelId
-    const channel = client.channels.cache.get(channelId)
-    if (channel === undefined) {
-      console.error(`Couldn't find the ${channelId} channel.`)
-      return
-    }
-    if (channel.isTextBased()) {
-      const tag = roleMention(streamAnnouncementsRoleId)
-      let message = `${tag} Adam just went live! <https://twitch.tv/AdamLearnsLive>`
-      if (title !== null) {
-        message += ` Title: ${title}`
-      }
-      await channel.send(message)
-    }
-  })
+  await clientWithCommands.login(process.env.DISCORD_TOKEN)
+}
 
-  clientWithCommands.on(Events.InteractionCreate, async (interaction) => {
+function onInteraction(clientWithCommands: ClientWithCommands) {
+  return async (interaction: Interaction) => {
     if (!interaction.isChatInputCommand()) return
 
     const command = clientWithCommands.Commands.get(interaction.commandName)
@@ -82,7 +72,41 @@ export async function init() {
             ephemeral: true,
           }))
     }
-  })
+  }
+}
 
-  await clientWithCommands.login(process.env.DISCORD_TOKEN)
+function onStreamOnline(
+  announcementsChannelId: string,
+  client: Client,
+  streamAnnouncementsRoleId: string,
+) {
+  return async (title: string | null) => {
+    console.log("Stream went online:", title)
+    const channelId = announcementsChannelId
+    const channel = client.channels.cache.get(channelId)
+    if (channel === undefined) {
+      console.error(`Couldn't find the ${channelId} channel.`)
+      return
+    }
+    if (channel.isTextBased()) {
+      const tag = roleMention(streamAnnouncementsRoleId)
+      let message = `${tag} Adam just went live! <https://twitch.tv/AdamLearnsLive>`
+      if (title !== null) {
+        message += ` Title: ${title}`
+      }
+      await channel.send(message)
+    }
+  }
+}
+
+function onClientReady(clientWithCommands: ClientWithCommands) {
+  return (readyClient: Client<true>) => {
+    const allGuildNames = readyClient.guilds.cache.map((guild) => guild.name)
+    const allGuildNamesString = JSON.stringify(allGuildNames)
+    const botName = readyClient.user.tag
+    const numCommands = clientWithCommands.Commands.size
+    console.log(
+      `Logged in on Discord as ${botName} to servers: ${allGuildNamesString}. Read in ${numCommands} command(s).`,
+    )
+  }
 }
