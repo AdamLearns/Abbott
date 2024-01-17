8f1e4e5d42b881067d6323a23de917d92d3a6f6c
Adam <adam@adamlearns.live>
2024-01-17 12:30:12 -0800

    Change some logging
    Added a line in one spot, lessened it in another.

--- src/twitch/Bot.ts
+++ src/twitch/Bot.ts
@@ -110,6 +110,7 @@ export class Bot {
 
       emitter.sendStreamOnline(title)
     })
+    console.log("Set up onStreamOnline listener from the Twitch bot")
   }
 
   async init() {
@@ -771,11 +772,7 @@ export class Bot {
     text: string,
     msg: ChatMessage,
   ) => {
-    console.info(
-      `Channel: ${channel}, User: ${user}, Text: ${text}, msg: ${JSON.stringify(
-        msg,
-      )}`,
-    )
+    console.info(`Channel: ${channel}, User: ${user}, Text: ${text}`)
 
     await this.processPotentialCommand(channel, user, text, msg)
   }
