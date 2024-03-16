82a0d51aa5bb1e7c70dfbbf42654f795b8390e1b
Adam <adam@adamlearns.live>
2024-03-16 10:21:59 -0700

    Handle stream being deleted entirely
    This used to just spam the console forever because it would think we're
    still online.

--- packages/youtube-api/src/api.ts
+++ packages/youtube-api/src/api.ts
@@ -102,11 +102,17 @@ export async function readChatMessages(
     const results = await google.youtube("v3").liveChatMessages.list(params)
     return results.data
   } catch (error) {
-    if (
-      error instanceof Common.GaxiosError &&
-      error.message.includes("The live chat is no longer live")
-    ) {
-      throw new StreamNoLongerLiveError()
+    if (error instanceof Common.GaxiosError) {
+      const { message } = error
+      if (
+        // When the stream goes offline (happens 5 minutes after the end of the
+        // broadcast)
+        message.includes("The live chat is no longer live") ||
+        // When the stream is deleted forever from the YouTube dashboard
+        message.includes("Live chat is not enabled for the specified broadcast")
+      ) {
+        throw new StreamNoLongerLiveError()
+      }
     }
 
     console.error("Error fetching chat messages", error)
